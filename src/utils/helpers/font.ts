14ef46d17af43cd1382ad3aad7348d19aef89c5b
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2026-01-07 22:45:31 +0700

    feat: update font layout

---
+++ src/utils/helpers/font.ts
@@ -0,0 +1,19 @@
+import localFont from "next/font/local";
+
+export const SFCompactRegular = localFont({
+  src: "./fonts/SF-Compact-Rounded-Regular.ttf",
+  display: "swap",
+  variable: "--font-sf-compact-regular",
+});
+
+export const SFCompactSemibold = localFont({
+  src: "./fonts/SF-Compact-Rounded-Semibold.ttf",
+  display: "swap",
+  variable: "--font-sf-compact-semibold",
+});
+
+export const SFCompactBold = localFont({
+  src: "./fonts/SF-Compact-Rounded-Bold.ttf",
+  display: "swap",
+  variable: "--font-sf-compact-bold",
+});
