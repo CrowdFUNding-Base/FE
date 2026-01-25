18dc02d834e923d1e4ca28257703101c6739a2cf
Galang Swastika Ramadhan <swastikaramadhan05@gmail.com>
2026-01-25 10:40:36 +0700

    feat(campaign card): add CampaignCard component

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,8 @@ export { Radio } from './element/Radio';
 export { default as WalletButton } from './element/WalletButton';
 export { default as CharityCard, CoinDisplay } from './element/CharityCard';
 export { default as LeaderboardCard, LeaderboardListItem } from './element/LeaderboardCard';
+export { CampaignCard } from './element/CampaignCard';
+export { CircularProgress } from './element/CircularProgress';
 
 export { default as DropdownMenu } from './layout/DropdownContainer';
 
